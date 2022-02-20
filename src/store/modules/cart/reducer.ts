dda2dc6d1a10c186991303e6f09f1adb5626fad0
Beatriz Murase <beatriz.murase@cobli.co>
2022-02-20 20:07:12 -0300

    create cart state types & initial state

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,3 +1,10 @@
-export function cart() {
-  return [];
+import { Reducer } from "redux";
+import { ICartState } from "./types";
+
+const INITIAL_STATE: ICartState = {
+  items: []
+};
+
+export const cart: Reducer<ICartState> = () => {
+  return INITIAL_STATE;
 }
