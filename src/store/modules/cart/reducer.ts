9fa393a31a7d0e1e31c0479652eea44f88e43cca
Beatriz Murase <beatriz.murase@cobli.co>
2022-02-20 23:21:25 -0300

    refactoring

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -7,33 +7,24 @@ const INITIAL_STATE: ICartState = {
 };
 
 export const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
-  switch(action.type) {
-    case 'ADD_PRODUCT_TO_CART': {
-      const { product } = action.payload;
-
-      // Immer tira a verbosidade que seria necessária para respeitar 
-      // a imutabilidade do estado
-      return produce(state, draft => {
+  // Immer tira a verbosidade que seria necessária para respeitar 
+  // a imutabilidade do estado
+  return produce(state, draft => {
+    switch(action.type) {
+      case 'ADD_PRODUCT_TO_CART': {
+        const { product } = action.payload;
+        
         draft.items.push({
           product,
           quantity: 1,
         });
-      });
 
-      // return {
-      //   ...state,
-      //   items: [
-      //     ...state.items,
-      //     {
-      //       product,
-      //       quantity: 1,
-      //     }
-      //   ]
-      // }
-    }
+        break;
+      }
 
-    default: {
-      return state;
+      default: {
+        return draft;
+      }
     }
-  }
+  });
 }
