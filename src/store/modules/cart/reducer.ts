f5e6ccc338cfa8e049689d0ad8cca4505e944ae1
Beatriz Murase <beatriz.murase@cobli.co>
2022-02-20 23:16:43 -0300

    refactor state update with immer

--- src/store/modules/cart/reducer.ts
+++ src/store/modules/cart/reducer.ts
@@ -1,5 +1,5 @@
-import { stat } from "fs";
 import { Reducer } from "redux";
+import produce from 'immer';
 import { ICartState } from "./types";
 
 const INITIAL_STATE: ICartState = {
@@ -10,16 +10,26 @@ export const cart: Reducer<ICartState> = (state = INITIAL_STATE, action) => {
   switch(action.type) {
     case 'ADD_PRODUCT_TO_CART': {
       const { product } = action.payload;
-      return {
-        ...state,
-        items: [
-          ...state.items,
-          {
-            product,
-            quantity: 1,
-          }
-        ]
-      }
+
+      // Immer tira a verbosidade que seria necessária para respeitar 
+      // a imutabilidade do estado
+      return produce(state, draft => {
+        draft.items.push({
+          product,
+          quantity: 1,
+        });
+      });
+
+      // return {
+      //   ...state,
+      //   items: [
+      //     ...state.items,
+      //     {
+      //       product,
+      //       quantity: 1,
+      //     }
+      //   ]
+      // }
     }
 
     default: {
