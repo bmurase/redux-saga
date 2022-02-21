b8b096b4832f01456a78d4f2a82b0f88de232849
Beatriz Murase <beatriz.murase@cobli.co>
2022-02-20 23:42:33 -0300

    creating catalog item component

--- src/components/Catalog.tsx
+++ src/components/Catalog.tsx
@@ -1,12 +1,10 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
 
-import { api } from '../services/api';
-import { addProductToCart } from '../store/modules/cart/actions';
 import { IProduct } from '../store/modules/cart/types';
+import { api } from '../services/api';
+import { CatalogItem } from './CatalogItem';
 
 export const Catalog: React.FC = () => {
-  const dispatch = useDispatch();
   const [catalog, setCatalog] = useState<IProduct[]>([]);
 
   useEffect(() => {
@@ -15,23 +13,12 @@ export const Catalog: React.FC = () => {
     });
   }, []);
 
-  const handleAddProductToCart = useCallback((product) => {
-    dispatch(addProductToCart(product));
-  }, []);
-
   return (
     <main>
       <h1>Catalog</h1>
 
       {catalog.map(product => (
-        <article key={product.id}>
-          <strong>{product.title}</strong> {" - "}
-          <span>{product.price}</span> {" "}
-
-          <button type="button" onClick={() => handleAddProductToCart(product)}>
-            Comprar
-          </button>
-        </article>
+        <CatalogItem key={product.id} product={product} />
       ))}
     </main>
   );
