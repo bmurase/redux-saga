2731d7c91fe350ea880cfa368245a1f77237f0c3
Beatriz Murase <beatriz.murase@cobli.co>
2022-02-27 19:02:04 -0300

    get current product quantity in cart

--- src/store/modules/cart/sagas.ts
+++ src/store/modules/cart/sagas.ts
@@ -1,10 +1,19 @@
-import { all, takeLatest } from 'redux-saga/effects';
+import { all, select, takeLatest } from "redux-saga/effects";
+import { IState } from "../..";
+import { addProductToCart } from "./actions";
 
-function checkProductStock() {
-  console.log('adicionou ao carrinho');
-  
+type CheckProductStockRequest = ReturnType<typeof addProductToCart>;
+
+function* checkProductStock({ payload }: any) {
+  const { product } = payload;
+  const currentQuantityInCart: number = yield select((state: IState) => {
+    return (
+      state.cart.items.find((item) => item.product.id === product.id)
+        ?.quantity ?? 0
+    );
+  });
+
+  console.log(currentQuantityInCart);
 }
 
-export default all([
-  takeLatest('ADD_PRODUCT_TO_CART', checkProductStock)
-]);
+export default all([takeLatest("ADD_PRODUCT_TO_CART", checkProductStock)]);
